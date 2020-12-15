fecfd1a1e74ae032dbe87cfe5387ececd52597b6
unknown <marco.bejarano03@gmail.com>
2020-12-15 01:47:20 -0800

    updated files

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        console.log(`Employee name: ${this.name}`);
+    }
+
+    getId() {
+        console.log(`Employee ID: ${this.id}`);
+    }
+
+    getEmail() {
+        console.log(`Employee Email: ${this.email}`);
+    }
+
+    getRole() {
+        
+    }
+}
+
+module.exports = Employee;
